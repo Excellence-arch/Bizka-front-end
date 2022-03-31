f8e31dd428c0f3f5384ebea189b5ef2b557565e6
excellence-arch <oladipupomichael9@gmail.com>
2022-03-31 13:32:43 +0100

    I want to start with the login and sign up pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<p>Hi</p>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" />
+        <Route path="/register"/>
       </Routes>
     </>
   )
